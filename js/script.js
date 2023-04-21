83468f87dcdfeaf6a6bf59a21ec88934ff8abb74
SharpIce <VupRbl@163.com>
2023-04-21 12:58:52 +0800

    优化页面加载
    签字人：SharpIce <VupRbl@163.com>

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
 console.log("欢迎访问 SharpIce 的个人站点👋");
 
+window.onerror = function (message, source, lineno, colno, error) {
+    if (error != null) {
+        SharpIce.DiaLog.Error('消息：' + message +
+            '</br></br>位置：' + source +
+            '</br></br>行：' + lineno +
+            '</br></br>列：' + colno +
+            '</br></br>错误：' + error);
+    }
+}
+
 var SharpIce = {
     Pages: {
-        PartiallyLoadingPagesBody: function (Pages) {
-            $.ajax({
-                url: "/pages/loading.html",
-                type: "get",
-                success: function (res) {
-                    $("body").html($(res));
-                },
-            });
+        PartiallyLoadingPagesContainer: function (Pages) {
+            SharpIce.Pages.PartiallyLoadingPages("/pages/loading.html", "#container");
             setTimeout(function () {
-                $.ajax({
-                    url: "/pages/" + Pages + ".html",
-                    type: "get",
-                    success: function (res) {
-                        $("body").html($(res));
-                    },
-                });
-            }, 1000)
+                SharpIce.Pages.PartiallyLoadingPages("/pages/" + Pages + ".html", "#container");
+            }, 100)
         },
+        PartiallyLoadingPages: function (PagesLocation, Content) {
+            $.ajax({
+                url: PagesLocation,
+                type: "get",
+                success: function (data) {
+                    $(Content).html($(data));
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    SharpIce.DiaLog.Error('消息：Ajax出错' +
+                        '</br></br>列：' + errorThrown +
+                        '</br></br>行：' + textStatus +
+                        '</br></br>错误信息：<pre style="text-align: left; background-color: mediumspringgreen; border-radius: 1rem; padding: 1rem;">' + JSON.stringify(jqXHR, null, 4) + '</pre>');
+                    throw null;
+                }
+            });
+        }
     },
+    DiaLog: {
+        Error: function (ErrorMessage) {
+            Swal.mixin({
+                position: 'top' // 设置顶部居中
+            }).fire({ // 内容
+                icon: 'error',
+                title: '出错啦Xwx',
+                html: ErrorMessage,
+            });
+        }
+    }
 };
