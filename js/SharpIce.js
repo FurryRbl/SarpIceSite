6ab8dca799f3b9286fccbf4f6b0a07a2419486be
SharpIce <VupRbl@163.com>
2023-04-27 23:08:32 +0800

    优化JavaScript
    签字人：SharpIce <VupRbl@163.com>

--- js/SharpIce.js
+++ js/SharpIce.js
@@ -1,70 +1,76 @@
-var SharpIce = {
+const SharpIce = {
     Pages: {
-        PartiallyLoadingPagesContainer: function (Pages) {
-            SharpIce.Pages.PartiallyLoadingPages("/pages/" + Pages + ".html", "#container");
+        PartiallyLoadingPagesContainer: (Pages) => {
+            SharpIce.Pages.PartiallyLoadingPages(`/pages/${Pages}.html`, "#container");
         },
-        PartiallyLoadingPages: function (PagesLocation, Content) {
-            $("body").stop(true, false); // 中断所有动画，防止出现渲染滞留
-            $('#Loading').css('visibility', 'unset');
-            $.ajax({
+        PartiallyLoadingPages: (PagesLocation, Content) => {
+            jQuery("body").stop(true, false); // 中断所有动画，防止出现渲染滞留
+            jQuery('#Loading').css('visibility', 'unset');
+            jQuery.ajax({
                 url: PagesLocation,
+                timeout: 60000,
                 type: "get",
-                success: function (data) {
-                    $(Content).html($(data));
+                success: (data) => {
+                    jQuery(Content).empty().append(jQuery(data));
                     GlobalReloadFunctions();
-                    $('#Loading').css('visibility', 'hidden');
+                    jQuery('#Loading').css('visibility', 'hidden');
                 },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    SharpIce.DiaLog.Error('Ajax出错' +
-                        '</br></br>错误信息：<pre style="text-align: left; background-color: mediumspringgreen; border-radius: 1rem; padding: 1rem;">' +
-                        JSON.stringify(jqXHR, null, 4) +
-                        '</pre>');
-                    $('#Loading').css('visibility', 'hidden');
+                error: (jqXHR, textStatus, errorThrown) => {
+                    SharpIce.ErrorProcessing.AjaxError(jqXHR);
                     throw null;
                 }
             });
         }
     },
     DiaLog: {
-        Error: function (ErrorMessage) {
+        Error: (ErrorMessage) => {
             Swal.fire({ // 对话框内容
                 icon: 'error',
                 title: '出错啦Xwx',
                 html: ErrorMessage,
                 heightAuto: false,
+                confirmButtonText: "确定",
             });
         },
-        ShowFileContentInDialog: function (FileLocation) { // 获取文件内容并显示在对话框上
-            $('#Loading').css('visibility', 'unset');
-            $.ajax({
+        ShowFileContentInDialog: (FileLocation) => { // 获取文件内容并显示在对话框上
+            jQuery('#Loading').css('visibility', 'unset');
+            jQuery.ajax({
                 url: FileLocation,
+                timeout: 60000,
+                type: "get",
                 dataType: "text",
-                success: function (data) {
+                success: (data) => {
                     Swal.fire({
                         title: "许可证",
-                        html: '<div class="CustomizeSlider" style="height: 20rem; white-space: pre-wrap;">' + data + '</div>',
+                        html: `<div class="CustomizeSlider" style="height: 20rem; white-space: pre-wrap;">${data}</div>`,
                         confirmButtonText: "确定",
                         heightAuto: false, // 禁止自动高度
                         width: 'auto', // 自动宽度
                     });
                     GlobalReloadFunctions();
-                    $('#Loading').css('visibility', 'hidden');
+                    jQuery('#Loading').css('visibility', 'hidden');
                 },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    SharpIce.DiaLog.Error('Ajax出错' +
-                        '</br></br>错误信息：<pre style="text-align: left; background-color: mediumspringgreen; border-radius: 1rem; padding: 1rem;">' +
-                        JSON.stringify(jqXHR, null, 4) +
-                        '</pre>');
-                    $('#Loading').css('visibility', 'hidden');
+                error: (jqXHR, textStatus, errorThrown) => {
+                    SharpIce.ErrorProcessing.AjaxError(jqXHR);
                     throw null;
                 }
             });
+        }
+    },
+    ErrorProcessing: {
+        AjaxError: (jqXHR) => {
+            SharpIce.DiaLog.Error(`
+            Ajax出错
+            <h4 style="text-align: left;">错误信息：</h4>
+            <pre style="text-align: left; background-color: mediumspringgreen; border-radius: 1rem; padding: 1rem;" class="CustomizeSlider">${JSON.stringify(jqXHR, null, 4)}</pre>
+            `);
+            jQuery('#Loading').css('visibility', 'hidden');
         }
     }
 };
 
 function GlobalReloadFunctions() {
-    $(".CustomizeSlider").mCustomScrollbar({ // 为CustomizeSlider类设置mCustomScrollbar的自定义滚动条
-        theme: "light"
+    jQuery(".CustomizeSlider").mCustomScrollbar({ // 为CustomizeSlider类设置mCustomScrollbar的自定义滚动条
+        theme: "dark"
     });
 }
--- js/script.js
+++ js/script.js
@@ -3,14 +3,22 @@ console.log("欢迎访问 SharpIce 的个人站点👋");
 // 错误回调
 window.onerror = function (message, source, lineno, colno, error) {
     if (error != null) { // 用于判断自定义错误是否返回null，自定义错误会自行处理。
-        SharpIce.DiaLog.Error('消息：' + message +
-            '</br></br>位置：' + source +
-            '</br></br>行：' + lineno +
-            '</br></br>列：' + colno +
-            '</br></br>错误：' + error);
+        SharpIce.DiaLog.Error(`
+        <div style="text-align: left; background-color: mediumspringgreen; border-radius: 1rem; padding: 1rem;">
+            消息：${message}
+            </br></br>
+            位置：${source}
+            </br></br>
+            行：${lineno}
+            </br></br>
+            列：${colno}
+            </br></br>
+            错误：${error}
+        </div>
+        `);
     }
 }
 
-$(window).on("load", function(){ //页面渲染完成后回调
+jQuery(window).on("load", () => { //页面渲染完成后回调
     GlobalReloadFunctions();
 });
